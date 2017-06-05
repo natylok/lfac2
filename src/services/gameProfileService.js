ae1a4b36c6fd05d9f724defd7f624c0c16946d27
Nati Levy <nati@cooladata.com>
2017-06-05 10:08:17 +0300

    fixed profile

--- src/services/gameProfileService.js
+++ src/services/gameProfileService.js
@@ -1,21 +1,34 @@
 let gameProfileService = {
-    getLolRankOptions: function(){
-        return [
-            { text: 'bronze', value: 'bronze', key: 'bronze'},
-            { text: 'silver', value: 'silver', key: 'silver' },
-            { text: 'gold', value: 'gold', key: 'gold'},
-            { text: 'platinum', value: 'platinum', key: 'platinum' },
-            { text: 'daimond', value: 'daimond', key:'daimond'}
-        ];
+    lol : {
+        getLolRankOptions: function(){
+            return [
+                { text: 'bronze', value: 'bronze', key: 'bronze'},
+                { text: 'silver', value: 'silver', key: 'silver' },
+                { text: 'gold', value: 'gold', key: 'gold'},
+                { text: 'platinum', value: 'platinum', key: 'platinum' },
+                { text: 'daimond', value: 'daimond', key:'daimond'}
+            ];
+        },
+        getLolRoles: function () {
+            return [
+                { text: 'ADC', value: 'ADC', key: 'ADC' },
+                { text: 'Mid', value: 'Mid', key: 'Mid' },
+                { text: 'Top', value: 'Top', key: 'Top' },
+                { text: 'Support', value: 'Support', key: 'Support' },
+                { text: 'Jungle', value: 'Jungle', key: 'Jungle' }
+            ];
+        },
+        getServerList: function(){
+            return [
+                { text: 'Europe East', value: 'europeEast', key: 'europeEast' },
+                { text: 'Europe West', value: 'europeWest', key: 'europeWest' },
+                { text: 'North America', value: 'NA', key: 'NA' }
+            ]
+        }
     },
-    getLolRoles: function () {
-        return [
-            { text: 'ADC', value: 'ADC', key: 'ADC' },
-            { text: 'Mid', value: 'Mid', key: 'Mid' },
-            { text: 'Top', value: 'Top', key: 'Top' },
-            { text: 'Support', value: 'Support', key: 'Support' },
-            { text: 'Jungle', value: 'Jungle', key: 'Jungle' }
-        ];
+    wow: {
+        getRoleList: function(){return [];},
+        getServerList: function () { return []; }
     }
 }
 
--- src/staticData/gameProfileData.js
+++ src/staticData/gameProfileData.js
@@ -5,15 +5,20 @@ GameProfileData.lol = {
     data: {
         nameInGame:'',
         rank:'',
-        mainRole:''
+        mainRole:'',
+        isLookingForClan:true
     },
     options: [
         { inputData: 'nameInGame', type:'input' , label:'Player Name:' , placeholder: 'Select your player name' },
-        { inputData: 'rank', type:'select', options:gameProfileService.getLolRankOptions() , label: 'Rank:', placeholder: 'Select your player rank' },
-        { inputData: 'mainRole', type: 'select', options: gameProfileService.getLolRoles(), label: 'Main Role:', placeholder: 'Select your main role' },
+        { inputData: 'rank', type: 'select', options: gameProfileService.lol.getServerList(), label: 'Server:', placeholder: 'Select the server you are playing at' },
+        { inputData: 'rank', type:'select', options:gameProfileService.lol.getLolRankOptions() , label: 'Rank:', placeholder: 'Select your player rank' },
+        { inputData: 'mainRole', type: 'select', options: gameProfileService.lol.getLolRoles(), label: 'Main Role:', placeholder: 'Select your main role' },
+        { inputData: 'isLookingForClan', type: 'select', options: [{ text: 'yes', value: true, key: 'true' }, { text: 'No', value: false, key: 'false' }] , label:'Looking for clan?'},
         { type: 'submit', text: 'Add new profile'},
     ]
 };
+
+
 
 
 export default GameProfileData;
